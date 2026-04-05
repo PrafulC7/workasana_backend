b3f1efeefbb7c4287947275efdc54138f3240217
PrafulC7 <prafullacharde00715@gmail.com>
2026-04-06 03:03:19 +0530

    connection error problem fixed

--- db/db.connect.js
+++ db/db.connect.js
@@ -4,11 +4,16 @@ dns.setServers(["1.1.1.1", "8.8.8.8"])
 require("dotenv").config();
 const mongoUri = process.env.MONGODB
 const initializeDatabase = async () => {
-    try{
-await mongoose.connect(mongoUri);
-    console.log("Connected to Database");
-}catch(error){
-    console.log("Error connecting to database", error)
+  try {
+    if (!process.env.MONGODB) {
+      throw new Error("MONGODB URI is missing");
     }
+
+    await mongoose.connect(process.env.MONGODB);
+
+    console.log("Connected to Database");
+  } catch (error) {
+    console.error("DB Connection Error:", error.message);
+  }
 };
 module.exports = { initializeDatabase }
